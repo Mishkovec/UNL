4bcb7d88cc22debb8870ed3f28c4d637f15b2b46
Mishkovec <neilsa@mail.ru>
2019-08-29 21:02:21 +0300

    feat: create adaptive design

---
+++ index.js
@@ -0,0 +1,16 @@
+$('.single-item').slick(
+    // {
+//     infinite: true,
+// 	dots: true,
+// 	slidesToShow: 2,
+// 	slidesToScroll: 1,
+//     responsive: [
+// 	    {
+// 	      breakpoint: 768,
+// 	      settings: {
+// 	        draggable : false,
+// 	      }
+//         }
+//     ]
+// }   
+);
