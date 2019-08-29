f1f903399744005b67ac6822a65ff438be25dcd5
Mishkovec <neilsa@mail.ru>
2019-08-29 21:41:53 +0300

    feat: add slick slider

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 $('.single-item').slick(
-    // {
-//     infinite: true,
-// 	dots: true,
-// 	slidesToShow: 2,
-// 	slidesToScroll: 1,
-//     responsive: [
-// 	    {
-// 	      breakpoint: 768,
-// 	      settings: {
-// 	        draggable : false,
-// 	      }
-//         }
-//     ]
-// }   
+    {
+    infinite: true,
+	dots: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+    responsive: [
+	    {
+	      breakpoint: 768,
+	      settings: {
+	        draggable : false,
+	      }
+        }
+    ]
+}   
 );
